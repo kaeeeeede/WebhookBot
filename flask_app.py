fcfa4c79ee9a7466443fe51ec39f791d7e7163c6
kaeeeeede <97236846+kaeeeeede@users.noreply.github.com>
2022-08-31 01:26:28 +0800

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object("db_config")
 db = SQLAlchemy(app)
-ENDPOINT_MAX_LENGTH = 25
+ENDPOINT_MAX_LENGTH = 200
 
 @app.route("/")
 def main():
